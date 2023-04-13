69cdfb99d0f37c37f8843a3fdbeb1343b753ba74
Amin <khai21112002@gmail.com>
2023-04-13 15:48:50 +0700

    dungmoi

--- app/src/main/java/com/example/deomo/MainActivity.java
+++ app/src/main/java/com/example/deomo/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // troi dep the
         setContentView(R.layout.activity_main);
     }
 }
